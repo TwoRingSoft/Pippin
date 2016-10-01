cb6a522dda970e0722100c8ce8bf78c8f513f5a1
Andrew McKnight <amcknight2718@gmail.com>
2016-10-01 18:35:19 -0400

    add utility to return a custom font or system font if not present

---
+++ UIFont/MaybeFont.swift
@@ -0,0 +1,22 @@
+//
+//  MaybeFont.swift
+//  Trigonometry
+//
+//  Created by Andrew McKnight on 10/1/16.
+//  Copyright © 2016 Two Ring Software. All rights reserved.
+//
+
+import UIKit
+
+extension UIFont {
+
+    /// - returns: either the UIFont defined by the family string provided, or the system font of the same size
+    static func maybeFont(ofFamily family: String, size: CGFloat) -> UIFont {
+        if let font = UIFont.init(name: family, size: size) {
+            return font
+        } else {
+            return UIFont.systemFont(ofSize: size)
+        }
+    }
+
+}
