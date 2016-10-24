45fa8ce5f35c5c940442acd74d472f6851d8cc20
Andrew McKnight <amcknight2718@gmail.com>
2016-10-24 09:16:47 -0400

    add some color utilities

---
+++ UIColor/MoreColors.swift
@@ -0,0 +1,19 @@
+//
+//  MoreColors.swift
+//  McMileage
+//
+//  Created by Andrew McKnight on 10/23/16.
+//  Copyright © 2016 andrew mcknight. All rights reserved.
+//
+
+import Foundation
+
+extension UIColor {
+
+    static var darkGreen: UIColor {
+        get {
+            return UIColor(red: 0, green: 0.5, blue: 0, alpha: 1)
+        }
+    }
+
+}
