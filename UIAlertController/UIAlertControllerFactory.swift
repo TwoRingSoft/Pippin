0f105a81715fdc5db0b6f8f698ece426515750e8
Andrew McKnight <amcknight2718@gmail.com>
2016-11-01 01:58:31 -0400

    add function to show an alert with "OK" and optionally "Cancel"

--- UIAlertController/UIAlertControllerFactory.swift
+++ UIAlertController/UIAlertControllerFactory.swift
@@ -11,11 +11,29 @@ import Foundation
 extension UIViewController {
 
     func confirmDestructiveAction(actionName: String, message: String, cancelLabel: String, actionBlock: @escaping ((Void) -> Void)) {
-        let alert = UIAlertController(title: "Confirm \(actionName)", message: message, preferredStyle: .actionSheet)
-        alert.addAction(UIAlertAction(title: "Yes, \(actionName)", style: .destructive, handler: { action in
-            actionBlock()
+        showCancellableAlert(withTitle: "Confirm \(actionName)", message: message, confirmTitle: "Yes, \(actionName)", cancelTitle: cancelLabel, style: .actionSheet, completion: { confirmed in
+            if confirmed {
+                actionBlock()
+            }
+        })
+    }
+
+    func showAlert(withTitle: String, message: String, confirmTitle: String = "OK", completion: ((Void) -> Void)?) {
+        let alert = UIAlertController(title: title, message: message, preferredStyle: .alert)
+        alert.addAction(UIAlertAction(title: confirmTitle, style: .destructive, handler: { action in
+            completion?()
         }))
-        alert.addAction(UIAlertAction(title: cancelLabel, style: .cancel, handler: nil))
+        present(alert, animated: true, completion: nil)
+    }
+
+    func showCancellableAlert(withTitle: String, message: String, confirmTitle: String = "OK", cancelTitle: String = "Cancel", style: UIAlertControllerStyle = .alert, completion: ((Bool) -> Void)?) {
+        let alert = UIAlertController(title: title, message: message, preferredStyle: style)
+        alert.addAction(UIAlertAction(title: confirmTitle, style: .destructive, handler: { action in
+            completion?(true)
+        }))
+        alert.addAction(UIAlertAction(title: cancelTitle, style: .cancel, handler: { action in
+            completion?(false)
+        }))
         present(alert, animated: true, completion: nil)
     }
     
