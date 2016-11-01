aa35c6a2ffb8531962400278b5b46a583b53cd62
Andrew McKnight <amcknight2718@gmail.com>
2016-11-01 01:03:13 -0400

    add geometric operations on CGRect

---
+++ CGRect/Operations.swift
@@ -0,0 +1,26 @@
+//
+//  Operations.swift
+//  McMileage
+//
+//  Created by Andrew McKnight on 10/31/16.
+//  Copyright © 2016 andrew mcknight. All rights reserved.
+//
+
+import Foundation
+
+extension CGRect {
+
+    func subtracting(other: CGRect, edge: CGRectEdge) -> CGRect {
+        let intersection = self.intersection(other)
+
+        if intersection.isNull { return self }
+
+        let chopAmount = (edge == .minXEdge || edge == .maxXEdge) ? intersection.width : intersection.height
+        return self.divided(atDistance: chopAmount, from: edge).remainder
+    }
+
+    func insetting(by insets: UIEdgeInsets) -> CGRect {
+        return CGRect(x: origin.x + insets.left, y: origin.y + insets.top, width: width - insets.left - insets.right, height: height - insets.top - insets.bottom)
+    }
+
+}
