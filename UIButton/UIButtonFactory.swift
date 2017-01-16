21665c43d63c7cb864c090c0b61790b561e90f26
Andrew McKnight <amcknight2718@gmail.com>
2017-01-15 20:52:11 -0500

    add paramaters for target/selector to call on touchUpInside

--- UIButton/UIButtonFactory.swift
+++ UIButton/UIButtonFactory.swift
@@ -16,13 +16,18 @@ extension UIButton {
      - imageSetName: name of the image set in Asset catalog to use for `.Normal` state
      - emphasisSuffix: suffix to append to `imageSetName` to show for `.Highlighted` and `.Selected` states (defaults to empty string to use same image for everything)
      - tintColor: the tint color to use on the images (defaults to white)
+     - target: target to receive a `touchUpInside` event
+     - selector: function to call for a `touchUpInside` event
      */
-    class func button(withImageSetName imageSetName: String, emphasisSuffix: String = "", tintColor: UIColor = UIColor.white) -> UIButton {
+    class func button(withImageSetName imageSetName: String, emphasisSuffix: String = "", tintColor: UIColor = UIColor.white, target: Any? = nil, selector: Selector? = nil) -> UIButton {
         let button = UIButton(type: .custom)
         button.tintColor = tintColor
         button.setImage(UIImage(named: "\(imageSetName)"), for: .normal)
         button.setImage(UIImage(named: "\(imageSetName)\(emphasisSuffix)"), for: .highlighted)
         button.setImage(UIImage(named: "\(imageSetName)\(emphasisSuffix)"), for: .selected)
+        if selector != nil {
+            button.addTarget(target, action: selector!, for: .touchUpInside)
+        }
         return button
     }
 
