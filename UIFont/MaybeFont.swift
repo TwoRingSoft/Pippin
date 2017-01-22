501ff426d845d99bd941ccd1c18700bb2d1dd45a
Andrew McKnight <amcknight2718@gmail.com>
2017-01-22 17:53:05 -0500

    add option to use bold system font if custom font can't be loaded

--- UIFont/MaybeFont.swift
+++ UIFont/MaybeFont.swift
@@ -10,12 +10,20 @@ import UIKit
 
 extension UIFont {
 
+    /// - parameters:
+    /// - family: the custom font family to use, if possible
+    /// - size: size of the font
+    /// - boldFont: if `true` and the custom font can't be loaded, construct the system font using `UIFont.boldSystemFont`; uses `UIFont.systemFont` otherwise
     /// - returns: either the UIFont defined by the family string provided, or the system font of the same size
-    static func maybeFont(ofFamily family: String, size: CGFloat) -> UIFont {
+    static func maybeFont(ofFamily family: String, size: CGFloat, boldFont: Bool = false) -> UIFont {
         if let font = UIFont.init(name: family, size: size) {
             return font
         } else {
-            return UIFont.systemFont(ofSize: size)
+            if boldFont {
+                return UIFont.boldSystemFont(ofSize: size)
+            } else {
+                return UIFont.systemFont(ofSize: size)
+            }
         }
     }
 
