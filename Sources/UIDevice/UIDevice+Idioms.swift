9980f694f6761e81029737b2e6a6d3dae16830a8
Andrew McKnight <amcknight2718@gmail.com>
2017-03-14 20:41:54 -0400

    add uidevice idiom extension for isPhone

---
+++ Sources/UIDevice/UIDevice+Idioms.swift
@@ -0,0 +1,19 @@
+//
+//  UIDevice+Idioms.swift
+//  Dough
+//
+//  Created by Andrew McKnight on 3/14/17.
+//  Copyright © 2017 Two Ring Software. All rights reserved.
+//
+
+import UIKit
+
+extension UIDevice {
+
+    class var isPhone: Bool {
+        get {
+            return current.userInterfaceIdiom == UIUserInterfaceIdiom.phone
+        }
+    }
+
+}
