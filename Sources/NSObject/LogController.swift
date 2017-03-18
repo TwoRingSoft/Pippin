fe2555c4803b4023747320c9bc430482d37e7e12
Andrew McKnight <amcknight2718@gmail.com>
2017-03-18 14:54:18 -0400

    provide ways to interchange strings and log level enum

--- Sources/NSObject/LogController.swift
+++ Sources/NSObject/LogController.swift
@@ -19,6 +19,8 @@ enum LogLevel: Int {
     case warning
     case error
 
+    static func defaultLevel() -> LogLevel { return .info }
+
     func xcgLogLevel() -> XCGLogger.Level {
         switch self {
         case .unknown: return XCGLogger.Level.info // default to info
@@ -27,6 +29,71 @@ enum LogLevel: Int {
         case .info: return XCGLogger.Level.info
         case .warning: return XCGLogger.Level.warning
         case .error: return XCGLogger.Level.error
+        }
+    }
+
+}
+
+extension LogLevel: CustomDebugStringConvertible {
+
+    var debugDescription: String {
+        get {
+            let s = String(asRDNSForApp: "dough", domain: "log-level", subpaths: [ description ])
+            return s
+        }
+    }
+
+    /// This isn't actually a function in `CustomdebugStringConvertible` but it's provided for consistent API.
+    init?(debugDescription: String) {
+        if debugDescription == String(reflecting: LogLevel.unknown) {
+            self = .unknown
+        } else if debugDescription == String(reflecting: LogLevel.verbose) {
+            self = .verbose
+        } else if debugDescription == String(reflecting: LogLevel.debug) {
+            self = .debug
+        } else if debugDescription == String(reflecting: LogLevel.info) {
+            self = .info
+        } else if debugDescription == String(reflecting: LogLevel.warning) {
+            self = .warning
+        } else if debugDescription == String(reflecting: LogLevel.error) {
+            self = .error
+        } else {
+            return nil
+        }
+    }
+
+}
+
+extension LogLevel: CustomStringConvertible {
+
+    var description: String {
+        get {
+            switch self {
+            case .unknown: return "unknown"
+            case .verbose: return "verbose"
+            case .debug: return "debug"
+            case .info: return "info"
+            case .warning: return "warning"
+            case .error: return "error"
+            }
+        }
+    }
+
+    init?(_ description: String) {
+        if description == String(describing: LogLevel.unknown) {
+            self = .unknown
+        } else if description == String(describing: LogLevel.verbose) {
+            self = .verbose
+        } else if description == String(describing: LogLevel.debug) {
+            self = .debug
+        } else if description == String(describing: LogLevel.info) {
+            self = .info
+        } else if description == String(describing: LogLevel.warning) {
+            self = .warning
+        } else if description == String(describing: LogLevel.error) {
+            self = .error
+        } else {
+            return nil
         }
     }
 
