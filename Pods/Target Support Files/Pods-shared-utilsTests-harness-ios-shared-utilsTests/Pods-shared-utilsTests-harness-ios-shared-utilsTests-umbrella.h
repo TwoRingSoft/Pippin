c60ce67470a422f453a670a210622c62b8cfa654
Andrew McKnight <amcknight2718@gmail.com>
2017-07-08 18:20:06 -0400

    get tests running

---
+++ Pods/Target Support Files/Pods-shared-utilsTests-harness-ios-shared-utilsTests/Pods-shared-utilsTests-harness-ios-shared-utilsTests-umbrella.h
@@ -0,0 +1,16 @@
+#ifdef __OBJC__
+#import <UIKit/UIKit.h>
+#else
+#ifndef FOUNDATION_EXPORT
+#if defined(__cplusplus)
+#define FOUNDATION_EXPORT extern "C"
+#else
+#define FOUNDATION_EXPORT extern
+#endif
+#endif
+#endif
+
+
+FOUNDATION_EXPORT double Pods_shared_utilsTests_harness_ios_shared_utilsTestsVersionNumber;
+FOUNDATION_EXPORT const unsigned char Pods_shared_utilsTests_harness_ios_shared_utilsTestsVersionString[];
+
