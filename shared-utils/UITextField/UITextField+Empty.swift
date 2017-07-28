1c2123943ff508fe69adf308c9e2f2a374d792d0
Andrew McKnight <amcknight2718@gmail.com>
2017-07-27 22:31:49 -0400

    add utilities for checking the emptiness of a text field

---
+++ shared-utils/UITextField/UITextField+Empty.swift
@@ -0,0 +1,24 @@
+//
+//  UITextField+Empty.swift
+//  InformedConsent
+//
+//  Created by Andrew McKnight on 7/27/17.
+//  Copyright © 2017 Insight Therapeutics. All rights reserved.
+//
+
+import UIKit
+
+extension UITextField {
+
+    var isEmpty: Bool {
+        return text == nil || text == ""
+    }
+
+    /**
+     By default, `UITextField` will return an empty string `""` if `text` is `nil`. This avoids getting an empty string for a textfield that really has nil text.
+     */
+    var nonemptyText: String? {
+        return isEmpty ? nil : text
+    }
+
+}
