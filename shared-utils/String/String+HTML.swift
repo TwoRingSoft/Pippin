475d468667ee8ba21c677488c76e2c9289bbb40e
Andrew McKnight <amcknight2718@gmail.com>
2017-07-28 03:33:46 -0400

    some helpers to create html strings

---
+++ shared-utils/String/String+HTML.swift
@@ -0,0 +1,25 @@
+//
+//  String+HTML.swift
+//  InformedConsent
+//
+//  Created by Andrew McKnight on 7/28/17.
+//  Copyright © 2017 Insight Therapeutics. All rights reserved.
+//
+
+import Foundation
+
+extension String {
+
+    var paragraph: String {
+        return enclose(withTag: "p")
+    }
+
+    var bold: String {
+        return enclose(withTag: "b")
+    }
+
+    func enclose(withTag tag: String) -> String {
+        return "<\(tag)>\(self)</\(tag)>"
+    }
+
+    }
