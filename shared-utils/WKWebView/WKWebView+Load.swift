18f92f904b6eb57cdeed581a01472ceea95be638
Andrew McKnight <amcknight2718@gmail.com>
2017-07-28 14:16:51 -0400

    add extension for wkwebview to load an html file with optional start/finish stubs

---
+++ shared-utils/WKWebView/WKWebView+Load.swift
@@ -0,0 +1,30 @@
+//
+//  WKWebView+Load.swift
+//  InformedConsent
+//
+//  Created by Andrew McKnight on 7/28/17.
+//  Copyright © 2017 Insight Therapeutics. All rights reserved.
+//
+
+import Foundation
+import WebKit
+
+extension WKWebView {
+
+    func loadHTML(fromFile file: String, htmlPrologueFile: String? = nil, htmlEpilogueFile: String? = nil) throws {
+        var htmlComponents = [String]()
+
+        if let prologue = htmlPrologueFile {
+            try htmlComponents.append(String(contentsOfResource: prologue, withExtension: "html"))
+        }
+
+        try htmlComponents.append(String(contentsOfResource: file))
+
+        if let epilogue = htmlEpilogueFile {
+            try htmlComponents.append(String(contentsOfResource: epilogue, withExtension: "html"))
+        }
+
+        loadHTMLString(htmlComponents.joined(), baseURL: nil)
+    }
+
+}
