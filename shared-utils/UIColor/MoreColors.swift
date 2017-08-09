5b996e4b82d726d989a87a283d294139ee20a9dd
Andrew McKnight <amcknight2718@gmail.com>
2017-08-09 09:27:37 -0400

    add dark blue color

--- shared-utils/UIColor/MoreColors.swift
+++ shared-utils/UIColor/MoreColors.swift
@@ -14,6 +14,10 @@ extension UIColor {
         return UIColor(red: 0, green: 0.5, blue: 0, alpha: 1)
     }
 
+    static var darkBlue: UIColor {
+        return UIColor(red: 0, green: 0, blue: 0.5, alpha: 1)
+    }
+
     static var lightLightGray: UIColor {
         return UIColor(red: 0.9, green: 0.9, blue: 0.9, alpha: 1)
     }
