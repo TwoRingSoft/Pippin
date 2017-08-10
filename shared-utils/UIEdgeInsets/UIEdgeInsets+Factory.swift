864af7832d375ea3db67aba07fb5e1a476778efa
Andrew McKnight <amcknight2718@gmail.com>
2017-08-10 09:34:54 -0400

    add new edge inset init and rename last one

--- shared-utils/UIEdgeInsets/UIEdgeInsets+Factory.swift
+++ shared-utils/UIEdgeInsets/UIEdgeInsets+Factory.swift
@@ -9,12 +9,19 @@
 import UIKit
 
 extension UIEdgeInsets {
-    
-    init(allEqualling value: CGFloat) {
+
+    init(all value: CGFloat) {
         self.top = value
         self.bottom = value
         self.left = value
         self.right = value
+    }
+
+    init(horizontal: CGFloat, vertical: CGFloat) {
+        self.top = vertical
+        self.bottom = vertical
+        self.left = horizontal
+        self.right = horizontal
     }
 
 }
