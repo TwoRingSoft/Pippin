52a8883ea98d4ac93b0446f1dc26d99f4a5323e5
Andrew McKnight <amcknight2718@gmail.com>
2017-11-27 17:40:36 -0700

    add AuthorizedAVCaptureDevice

---
+++ Sources/Pippin/AVCaptureDevice/AuthorizedAVCaptureDevice.swift
@@ -0,0 +1,116 @@
+//
+//  AuthorizedAVCaptureDevice.swift
+//  Augre
+//
+//  Created by Andrew McKnight on 4/18/16.
+//  Copyright © 2016 Two Ring Software. All rights reserved.
+//
+
+import AVFoundation
+import Foundation
+
+enum MediaType {
+    case video
+    case audio
+    case text
+    case closedCaption
+    case subtitle
+    case timecode
+    case metadata
+    case muxed
+
+    func stringConstant() -> AVMediaType {
+        switch(self) {
+        case .audio:
+            return AVMediaType.audio
+        case .video:
+            return AVMediaType.video
+        case .text:
+            return AVMediaType.text
+        case .closedCaption:
+            return AVMediaType.closedCaption
+        case .subtitle:
+            return AVMediaType.subtitle
+        case .timecode:
+            return AVMediaType.timecode
+        case .metadata:
+            return AVMediaType.metadata
+        case .muxed:
+            return AVMediaType.muxed
+        }
+    }
+}
+
+private let CaptureDeviceAuthorizationErrorDomain = "com.tworingsoft.can-i-haz.authorized-capture-device.error"
+
+private enum CaptureDeviceAuthorizationErrorCode: Int {
+    case denied
+}
+
+typealias CaptureDeviceAuthorizationResult = (captureDevice: AVCaptureDevice?, authorizationError: NSError?)
+typealias CaptureDeviceAuthorizationCompletionBlock = ((CaptureDeviceAuthorizationResult) -> Void)
+
+final class AuthorizedAVCaptureDevice: NSObject {
+
+    class func authorizedCaptureDevice(_ mediaType: MediaType, completion: CaptureDeviceAuthorizationCompletionBlock?) {
+
+        #if (arch(i386) || arch(x86_64)) && os(iOS)
+            completion?((nil, NSError(domain: CaptureDeviceAuthorizationErrorDomain, code: CaptureDeviceAuthorizationErrorCode.denied.rawValue, userInfo: [NSLocalizedDescriptionKey: "Capture device not available on iOS Simulator."])))
+            return
+        #endif
+
+        var authorized = false
+        switch(AVCaptureDevice.authorizationStatus(for: mediaType.stringConstant())) {
+        case .authorized:
+            authorized = true
+            break
+        case .denied:
+            authorized = false
+            break
+        case .notDetermined:
+            authorized = false
+            break
+        case .restricted:
+            authorized = false
+            break
+        }
+
+        if authorized {
+            completion?(createCaptureDevice(mediaType))
+            return
+        }
+
+        checkRequiredPlistValues()
+
+        AVCaptureDevice.requestAccess(for: mediaType.stringConstant()) { authorizationGranted in
+            if !authorizationGranted {
+                let authorizationError: NSError? = NSError(domain: CaptureDeviceAuthorizationErrorDomain, code: CaptureDeviceAuthorizationErrorCode.denied.rawValue, userInfo: [NSLocalizedDescriptionKey: "Capture device for media type \(mediaType.stringConstant()) not available.", NSLocalizedFailureReasonErrorKey: "The user denied the request to access \(mediaType.stringConstant())."])
+                completion?((captureDevice: nil, authorizationError: authorizationError))
+                return
+            }
+
+            completion?(createCaptureDevice(mediaType))
+        }
+    }
+
+    fileprivate class func checkRequiredPlistValues() {
+        let requiredPlistKey = "NSCameraUsageDescription"
+
+        guard let infoDictionary = Bundle.main.infoDictionary else {
+            fatalError("Could not locate Info.plist keys and values.")
+        }
+        guard let requiredCameraRequestDescriptionValue = infoDictionary[requiredPlistKey] as? String else {
+            fatalError("You must provide a value for \(requiredPlistKey) in your app's Info.plist for the camera access request dialog to be shown to the user.")
+        }
+        if requiredCameraRequestDescriptionValue.count == 0 {
+            fatalError("You provided a description value for \(requiredPlistKey) with length 0. This string is displayed to the user in the request prompt; consider providing more information why you are requesting location services.")
+        }
+    }
+
+    fileprivate class func createCaptureDevice(_ mediaType: MediaType) -> CaptureDeviceAuthorizationResult {
+        let captureDevice = AVCaptureDevice.default(for: mediaType.stringConstant())
+        return (captureDevice: captureDevice, authorizationError: nil)
+    }
+
+}
+
