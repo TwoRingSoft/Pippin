27549a8249b6ddc43762d38e33d019d36ca692cb
Andrew McKnight <amcknight2718@gmail.com>
2017-11-30 21:02:02 -0700

    add file for testing with launch argument constant

---
+++ Testing/UITesting.swift
@@ -0,0 +1,10 @@
+//
+//  UITesting.swift
+//  Pippin
+//
+//  Created by Andrew McKnight on 11/30/17.
+//
+
+import Foundation
+
+public let UITestLaunchArgument = "com.tworingsoft.pippin.uitest.launch-argument"
