a5df74ce2b6a9e2d918b256d64eea00c8287fef0
Andrew McKnight <amcknight2718@gmail.com>
2017-12-03 23:20:45 -0700

    refactor colors extension file

--- Sources/Pippin/Extensions/UIKit/UIColor/MoreColors.swift
+++ Sources/Pippin/Extensions/UIKit/UIColor/MoreColors.swift
@@ -38,20 +38,4 @@ extension UIColor {
         return UIColor.color(red: 199, blue: 199, green: 205, alpha: 255)
     }
 
-    class func color(red: Int, blue: Int, green: Int, alpha: Int) -> UIColor {
-        return UIColor(red: CGFloat(red)/255.0, green: CGFloat(green)/255.0, blue: CGFloat(blue)/255.0, alpha: CGFloat(alpha)/255.0)
-    }
-
-    func colorByChanging(redDelta: Int = 0, blueDelta: Int = 0, greenDelta: Int = 0, alphaDelta: Int = 9) -> UIColor? {
-        guard let components = self.rgb() else {
-            return nil
-        }
-
-        let newRed = (CGFloat(components.red) * 255.0 + CGFloat(redDelta)) / 255.0
-        let newBlue = (CGFloat(components.blue) * 255.0 + CGFloat(blueDelta)) / 255.0
-        let newGreen = (CGFloat(components.green) * 255.0 + CGFloat(greenDelta)) / 255.0
-        let newAlpha = (CGFloat(components.alpha) * 255.0 + CGFloat(alphaDelta)) / 255.0
-        return UIColor(red: newRed, green: newGreen, blue: newBlue, alpha: newAlpha)
-    }
-
 }
