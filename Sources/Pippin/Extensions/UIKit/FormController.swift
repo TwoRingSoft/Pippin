c6ba5e7795e679d68acf2626f17a6dbda3f07c1c
Andrew McKnight <amcknight2718@gmail.com>
2017-12-07 12:23:53 -0700

    rework FormController to include UITextView

--- Sources/Pippin/Extensions/UIKit/FormController.swift
+++ Sources/Pippin/Extensions/UIKit/FormController.swift
@@ -11,22 +11,36 @@ import UIKit
 
 public class FormController: NSObject {
 
-    fileprivate var textFields: [UITextField]!
+    fileprivate var responders: [UIResponder]!
     fileprivate var oldTextFieldDelegates: [UITextField: UITextFieldDelegate?] = [:]
-    fileprivate var currentTextField: UITextField?
+    fileprivate var oldTextViewDelegates: [UITextView: UITextViewDelegate?] = [:]
+    fileprivate var currentResponder: UIResponder?
 
-    public init(textFields: [UITextField]) {
+    public init(responders: [UIResponder]) {
         super.init()
-        self.textFields = textFields
+        self.responders = responders
 
-        for textField in textFields {
-            oldTextFieldDelegates[textField] = textField.delegate
-            textField.delegate = self
-            textField.inputAccessoryView = accessoryViewForTextField(textField: textField)
-            if textFields.index(of: textField)! < textFields.count - 1 {
-                textField.returnKeyType = .next
+        for responder in responders {
+            if let textField = responder as? UITextField {
+                oldTextFieldDelegates[textField] = textField.delegate
+                textField.delegate = self
+                textField.inputAccessoryView = accessoryViewForResponder(responder: textField)
+                if responders.index(of: responder)! < responders.count - 1 {
+                    textField.returnKeyType = .next
+                } else {
+                    textField.returnKeyType = .done
+                }
+            } else if let textView = responder as? UITextView {
+                oldTextViewDelegates[textView] = textView.delegate
+                textView.delegate = self
+                textView.inputAccessoryView = accessoryViewForResponder(responder: textView)
+                if responders.index(of: responder)! < responders.count - 1 {
+                    textView.returnKeyType = .next
+                } else {
+                    textView.returnKeyType = .done
+                }
             } else {
-                textField.returnKeyType = .done
+                fatalError("Unsupported responder type.")
             }
         }
     }
@@ -37,21 +51,21 @@ public class FormController: NSObject {
 public extension FormController {
 
     func resignResponders() {
-        for responder in textFields {
+        for responder in responders {
             responder.resignFirstResponder()
         }
     }
 
-    func accessoryViewForTextField(textField: UITextField) -> UIView {
+    func accessoryViewForResponder(responder: UIResponder) -> UIView {
         let previousButton = UIBarButtonItem(title: "Prev", style: .plain, target: self, action: #selector(previousTextField))
-        if let idx = textFields.index(of: textField) {
+        if let idx = responders.index(of: responder) {
             previousButton.isEnabled = idx > 0
         } else {
             previousButton.isEnabled = false
         }
         let nextButton = UIBarButtonItem(title: "Next", style: .plain, target: self, action: #selector(nextTextField))
-        if let idx = textFields.index(of: textField) {
-            nextButton.isEnabled = idx < textFields.count - 1
+        if let idx = responders.index(of: responder) {
+            nextButton.isEnabled = idx < responders.count - 1
         } else {
             nextButton.isEnabled = false
         }
@@ -81,30 +95,46 @@ public extension FormController {
 
 }
 
+extension FormController: UITextViewDelegate {
+
+    public func textViewDidBeginEditing(_ textView: UITextView) {
+        currentResponder = textView
+        if let delegate = oldTextViewDelegates[textView], let unwrappedDelegate = delegate {
+            unwrappedDelegate.textViewDidBeginEditing?(textView)
+        }
+    }
+
+}
+
 extension FormController: UITextFieldDelegate {
 
     // MARK: UITextField traversal
 
     public func textFieldDidBeginEditing(_ textField: UITextField) {
-        currentTextField = textField
+        currentResponder = textField
+        if let delegate = oldTextFieldDelegates[textField], let unwrappedDelegate = delegate {
+            unwrappedDelegate.textFieldDidBeginEditing?(textField)
+        }
     }
 
     public func textField(_ textField: UITextField, shouldChangeCharactersIn range: NSRange, replacementString string: String) -> Bool {
-        if let delegate = oldTextFieldDelegates[textField], let unwrappedDelegate = delegate {
-            if unwrappedDelegate.responds(to: #selector(UITextFieldDelegate.textField(_:shouldChangeCharactersIn:replacementString:))) {
-                return unwrappedDelegate.textField!(textField, shouldChangeCharactersIn: range, replacementString: string)
-            }
+        if let delegate = oldTextFieldDelegates[textField], let unwrappedDelegate = delegate, let delegateResponse = unwrappedDelegate.textField?(textField, shouldChangeCharactersIn: range, replacementString: string) {
+            return delegateResponse
         }
 
         return true
     }
 
     public func textFieldShouldReturn(_ textField: UITextField) -> Bool {
-        let idx = textFields.index(of: textField)!
-        if idx == textFields.count - 1 {
+        let idx = responders.index(of: textField)!
+        if idx == responders.count - 1 {
             resignResponders()
         } else {
             nextTextField()
+        }
+
+        if let delegate = oldTextFieldDelegates[textField], let unwrappedDelegate = delegate, let delegateResponse = unwrappedDelegate.textFieldShouldReturn?(textField) {
+            return delegateResponse
         }
 
         return true
@@ -119,13 +149,13 @@ extension FormController: UITextFieldDelegate {
     }
 
     func nextTextField() {
-        let nextIdx = textFields.index(of: currentTextField!)! + 1
-        textFields[nextIdx].becomeFirstResponder()
+        let nextIdx = responders.index(of: currentResponder!)! + 1
+        responders[nextIdx].becomeFirstResponder()
     }
 
     func previousTextField() {
-        let previousIdx = textFields.index(of: currentTextField!)! - 1
-        textFields[previousIdx].becomeFirstResponder()
+        let previousIdx = responders.index(of: currentResponder!)! - 1
+        responders[previousIdx].becomeFirstResponder()
     }
 
 }
