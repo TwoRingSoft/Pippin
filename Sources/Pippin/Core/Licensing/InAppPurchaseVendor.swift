5342f4a7cf151ad4c2a6aa6db8b94fdb5ce4d544
Andrew McKnight <amcknight2718@gmail.com>
2018-01-26 10:15:24 -0700

    modify/add InAppPurchaseVendor functions
    - make completion for purchase @escaping
    - add function to restore purchases

--- Sources/Pippin/Core/Licensing/InAppPurchaseVendor.swift
+++ Sources/Pippin/Core/Licensing/InAppPurchaseVendor.swift
@@ -9,6 +9,7 @@ import Foundation
 
 public typealias IAPPurchaseCheckCompletion = ((_ purchased: Bool, _ error: Error?) -> (Void))
 public typealias IAPProductsCompletion<T> = ((_ products: [T], _ error: Error?) -> (Void)) where T: InAppPurchase
+public typealias IAPRestoredProductsCompletion = ((_ productIDs: [String], _ error: Error?) -> (Void))
 public typealias IAPPurchaseCompletion = ((_ error: Error?) -> (Void))
 
 public protocol InAppPurchase {
@@ -20,6 +21,7 @@ public protocol InAppPurchaseVendor {
 
     func fetchProducts<T>(completion: @escaping IAPProductsCompletion<T>) where T: InAppPurchase
     func checkLicense(forInAppPurchase identifier: String, completion: @escaping IAPPurchaseCheckCompletion)
-    func purchase<T>(product: T, completion: IAPPurchaseCompletion) where T: InAppPurchase
+    func purchase<T>(product: T, completion: @escaping IAPPurchaseCompletion) where T: InAppPurchase
+    func restorePurchases(completion: @escaping IAPRestoredProductsCompletion)
 
 }
