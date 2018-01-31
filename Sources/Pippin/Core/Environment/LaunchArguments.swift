f548b9e58e1bd894a99c242670d6010647d0c738
Andrew McKnight <amcknight2718@gmail.com>
2018-01-30 21:56:46 -0700

    add launch arguents for simulating in app purchase scenarios

--- Sources/Pippin/Core/Environment/LaunchArguments.swift
+++ Sources/Pippin/Core/Environment/LaunchArguments.swift
@@ -12,6 +12,10 @@ public enum LaunchArgument: String {
     case uiTest = "ui-test"
     case unlockIAP = "unlock-in-app-purchases"
     case simulateIAPFlow = "simulate-in-app-purchases-flow"
+    case simulateProductPurchaseSuccess = "simulate-product-purchase-success"
+    case simulateProductPurchaseFailure = "simulate-product-purchase-failure"
+    case simulateInAppPurchaseRestoreSuccess = "simulate-in-app-purchase-restore-success"
+    case simulateInAppPurchaseRestoreFailure = "simulate-in-app-purchase-restore-failure"
 
     public func activated() -> Bool {
         return ProcessInfo.processInfo.arguments.contains(String(describing: self))
