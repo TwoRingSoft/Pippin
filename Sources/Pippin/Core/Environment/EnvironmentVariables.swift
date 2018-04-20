93fef8ee76ab4c7867e18810c7b19f077edbc115
Andrew McKnight <amcknight2718@gmail.com>
2018-04-20 13:28:36 -0600

    add environment variable for simulated restored in app purchases

--- Sources/Pippin/Core/Environment/EnvironmentVariables.swift
+++ Sources/Pippin/Core/Environment/EnvironmentVariables.swift
@@ -7,10 +7,21 @@
 
 import Foundation
 
+/// An enumeration of possible keys that can be passed into a build of an app, either by using the Xcode Scheme > Run > Arguments > Environment Variables area, or on the command line when running `xcodebuild`.
 public enum EnvironmentVariable: String {
 
+    /// The string separator for simulated restored in app purchases, used in the value for `simulatedRestoredInAppPurchaseIdentifiers`.
+    public static let simulatedInAppPurchaseIdentifierSeparator = ":"
+
+    /// A log leve to use as an override to the default.
     case logLevel = "log-level"
 
+    /// A set of product IDs to use when simulating the restoration of in app purchases, to return as the set being restored.
+    case simulatedRestoredInAppPurchaseIdentifiers = "simulated-restored-in-app-purchase-identifiers"
+
+    /// Get the value passed in for the calling environment variable.
+    ///
+    /// - Returns: The string contents assigned to the calling environment variable, if one was provided.
     public func value() -> String? {
         return ProcessInfo.processInfo.environment[String(describing: self)] as String?
     }
