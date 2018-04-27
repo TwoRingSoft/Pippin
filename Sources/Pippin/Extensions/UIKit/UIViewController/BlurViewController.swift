583407f738e36bff659df53c517de4fa7cb2fccf
Andrew McKnight <amcknight2718@gmail.com>
2018-04-26 21:14:47 -0600

    parameterize blur style in BlurViewController
    add documentation comments

--- Sources/Pippin/Extensions/UIKit/UIViewController/BlurViewController.swift
+++ Sources/Pippin/Extensions/UIKit/UIViewController/BlurViewController.swift
@@ -8,18 +8,26 @@
 
 import UIKit
 
+/// A `UIViewController` subclass that displays a child view controller with an active blur background.
 public final class BlurViewController: UIViewController {
 
     public required init?(coder aDecoder: NSCoder) {
         super.init(coder: aDecoder)
     }
 
-    public required init(viewController: UIViewController) {
+    /// Instantiate a new `BlurViewController` with a child view controller and a blur style.
+    ///
+    /// - Parameters:
+    ///   - viewController: the view controller to show with a blur.
+    ///   - blurStyle: the type of blur to use.
+    ///
+    /// - Note: `viewController`'s subviews must have transparent background.
+    public required init(viewController: UIViewController, blurStyle: UIBlurEffectStyle = .light) {
         super.init(nibName: nil, bundle: nil)
 
         addNewChildViewController(newChildViewController: viewController)
 
-        let blurEffect = UIBlurEffect(style: .light)
+        let blurEffect = UIBlurEffect(style: blurStyle)
         let blurView = UIVisualEffectView(effect: blurEffect)
         blurView.contentView.addSubview(viewController.view)
         viewController.view.fillSuperview()
