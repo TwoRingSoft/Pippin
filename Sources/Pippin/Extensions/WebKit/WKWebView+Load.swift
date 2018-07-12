1479acb420584685372d142e9958532883a9476a
Andrew McKnight <amcknight2718@gmail.com>
2018-07-12 13:24:41 -0600

    parameterize bundle in which to search for HTML resource

--- Sources/Pippin/Extensions/WebKit/WKWebView+Load.swift
+++ Sources/Pippin/Extensions/WebKit/WKWebView+Load.swift
@@ -11,17 +11,17 @@ import WebKit
 
 public extension WKWebView {
 
-    func loadHTML(fromFile file: String, htmlPrologueFile: String? = nil, htmlEpilogueFile: String? = nil, baseURL: URL? = nil) throws {
+    func loadHTML(fromFile file: String, htmlPrologueFile: String? = nil, htmlEpilogueFile: String? = nil, baseURL: URL? = nil, bundle: Bundle = Bundle.main) throws {
         var htmlComponents = [String]()
 
         if let prologue = htmlPrologueFile {
-            try htmlComponents.append(String(contentsOfResource: prologue, withExtension: "html"))
+            try htmlComponents.append(String(contentsOfResource: prologue, withExtension: "html", inBundle: bundle))
         }
 
-        try htmlComponents.append(String(contentsOfResource: file))
+        try htmlComponents.append(String(contentsOfResource: file, inBundle: bundle))
 
         if let epilogue = htmlEpilogueFile {
-            try htmlComponents.append(String(contentsOfResource: epilogue, withExtension: "html"))
+            try htmlComponents.append(String(contentsOfResource: epilogue, withExtension: "html", inBundle: bundle))
         }
 
         loadHTMLString(htmlComponents.joined(), baseURL: baseURL)
