70744955bac2d0a7c800e36a765209be32142dd1
Andrew McKnight <amcknight2718@gmail.com>
2018-12-07 16:21:49 -0900

    allow UITableViewCell separators to optionally pin to the top

--- Sources/Pippin/Extensions/UIKit/UITableViewCell/UITableViewCell+Separators.swift
+++ Sources/Pippin/Extensions/UIKit/UITableViewCell/UITableViewCell+Separators.swift
@@ -16,16 +16,23 @@ public extension UITableViewCell {
     ///   - thickness: How thick the separator should be. Defaults to Pippin's `UIScreen.main.hairlineWidth`.
     ///   - backgroundColor: The background color of the separator. Default is `UIColor.black`.
     ///   - insets: The insets to use for positioning. `UIEdgeInsets.top` is ignored. Default is `UIEdgeInsets.zero`.
-    func addSeparator(thickness: CGFloat = UIScreen.main.hairlineWidth, backgroundColor: UIColor = .black, insets: UIEdgeInsets = .zero) {
+    ///   - pinnedToTop: if `false`, the separator is pinned to the bottom of the cell and `insets.bottom` is used for spacing; if `true`, the separator is pinned to the top of the cell and `insets.top` is used for spacing.
+    func addSeparator(thickness: CGFloat = UIScreen.main.hairlineWidth, backgroundColor: UIColor = .black, insets: UIEdgeInsets = .zero, pinnedToTop: Bool = false) {
         let separator = UIView(frame: .zero)
         separator.backgroundColor = backgroundColor
         contentView.addSubview(separator)
+        let verticalConstraint: NSLayoutConstraint
+        if pinnedToTop {
+            verticalConstraint = separator.topAnchor.constraint(equalTo: contentView.topAnchor, constant: insets.top)
+        } else {
+            verticalConstraint = separator.bottomAnchor.constraint(equalTo: contentView.bottomAnchor, constant: -insets.bottom)
+        }
         separator.translatesAutoresizingMaskIntoConstraints = false
         [
             separator.leadingAnchor.constraint(equalTo: contentView.leadingAnchor, constant: insets.left),
             separator.trailingAnchor.constraint(equalTo: contentView.trailingAnchor, constant: -insets.right),
             separator.heightAnchor.constraint(equalToConstant: thickness),
-            separator.bottomAnchor.constraint(equalTo: contentView.bottomAnchor, constant: -insets.bottom),
+            verticalConstraint,
         ].forEach { $0.isActive = true }
     }
     
