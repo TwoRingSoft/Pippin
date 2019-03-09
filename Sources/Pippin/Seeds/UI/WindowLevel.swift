aa6eb9e94cdeeefdfd639764d793b58de51e2cb6
Andrew McKnight <amcknight2718@gmail.com>
2019-03-08 23:54:32 -0900

    extract default WindowLevel to static constant

--- Sources/Pippin/Seeds/UI/WindowLevel.swift
+++ Sources/Pippin/Seeds/UI/WindowLevel.swift
@@ -10,11 +10,13 @@ import Foundation
 /// `enum` defining the `CGFloat` offsets to be added to the `UIWindowLevelAlert` level of the respective component's display `UIWindow`.
 /// 
 public enum WindowLevel: CGFloat {
+    static let `default` = UIWindow.Level.statusBar
+
     case debugging = 1
     case indicator = 2
     case alerter = 3
     
     public func windowLevel() -> UIWindow.Level {
-        return UIWindow.Level.statusBar
+        return WindowLevel.`default`
     }
 }
