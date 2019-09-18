5bb498c36072f6448750e303bb95302fabf4e341
Andrew McKnight <amcknight2718@gmail.com>
2019-09-17 21:47:52 -0800

    fix: set PippinTestHarness to swift 5, fix build

--- Pippin/PippinTestHarness/AppDelegate.swift
+++ Pippin/PippinTestHarness/AppDelegate.swift
@@ -49,7 +49,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         return environment
     }()
 
-    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
+    internal func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         environment.debugging?.installViews()
         environment.touchVisualizer?.installViews()
         return true
