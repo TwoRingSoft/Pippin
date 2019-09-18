74c29c7100ea48c7b4c3b477537824cd0a8a3560
Andrew McKnight <amcknight2718@gmail.com>
2019-09-17 21:22:08 -0800

    feat: make PippinTestHarness a real example app
    - set up a full Environment with debugging and touch visualization to
    demo all the components

--- Pippin/PippinTestHarness/AppDelegate.swift
+++ Pippin/PippinTestHarness/AppDelegate.swift
@@ -12,13 +12,75 @@ import UIKit
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
-
     var window: UIWindow?
+    
+    lazy var environment: Environment = {
+        let environment = Environment()
+        environment.crashReporter = CrashlyticsAdapter(debug: true)
+        environment.crashReporter?.environment = environment
+
+        environment.logger = XCGLoggerAdapter(name: "PippinTestHarnessLogger", logLevel: .debug)
+        environment.logger?.environment = environment
+
+        environment.activityIndicator = JGProgressHUDAdapter()
+        environment.activityIndicator?.environment = environment
+
+        environment.debugging = DebugFlowController(databaseFileName: "PippinTestHarnessDatabase", delegate: self, environment: environment )
+        environment.debugging?.environment = environment
+
+        environment.touchVisualizer = COSTouchVisualizerAdapter(rootViewController: ViewController(nibName: nil, bundle: nil))
+        environment.touchVisualizer?.environment = environment
+
+        environment.bugReporter = PinpointKitAdapter(recipients: ["andrew@tworingsoft.com"])
+        environment.bugReporter?.environment = environment
+
+        environment.alerter = SwiftMessagesAdapter()
+        environment.alerter?.environment = environment
+
+        AuthorizedCLLocationManager.authorizedLocationManager(scope: .whenInUse, completion: { (result) in
+            switch result {
+            case .success(let locationManager):
+                environment.locator = CoreLocationAdapter(authorizedLocationManager: locationManager, locatorDelegate: self)
+                environment.locator?.environment = environment
+            case .failure(let error):
+                fatalError("failed to authorize location manager: \(error)")
+            }
+        })
+        return environment
+    }()
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
-        // Override point for customization after application launch.
+        environment.debugging?.installViews()
+        environment.touchVisualizer?.installViews()
         return true
     }
-
 }
 
+extension AppDelegate: DebugFlowControllerDelegate {
+    func exportedDatabaseData() -> Data? {
+        environment.alerter?.showAlert(title: "Exporting database", message: "This is where your app would export its database to transport to other instances of itself, liek on another test device.", type: .info, dismissal: .interactive, occlusion: .weak)
+        return nil
+    }
+
+    func failedToExportDatabase(error: DebugFlowError) {
+        environment.alerter?.showAlert(title: "Failed exporting database", message: "Something went wrong.", type: .error, dismissal: .interactive, occlusion: .strong)
+    }
+
+    func debugFlowControllerWantsToGenerateTestModels(debugFlowController: Debugging) {
+        environment.alerter?.showAlert(title: "Generating test models", message: "This is where your app would generate some test model entities.", type: .info, dismissal: .interactive, occlusion: .strong)
+    }
+
+    func debugFlowControllerWantsToDeleteModels(debugFlowController: Debugging) {
+        environment.alerter?.showAlert(title: "Deleting models", message: "This is where your app would delete its model entities.", type: .info, dismissal: .interactive, occlusion: .strong)
+    }
+}
+
+extension AppDelegate: LocatorDelegate {
+    func locator(locator: Locator, updatedToLocation location: Location) {
+        environment.logger?.logInfo(message: "locator \(locator) updated to location \(location)")
+    }
+
+    func locator(locator: Locator, encounteredError: LocatorError) {
+        environment.logger?.logError(message: "Locator \(locator) encountered error \(encounteredError)", error: encounteredError)
+    }
+}
--- Pippin/PippinTestHarness/ViewController.swift
+++ Pippin/PippinTestHarness/ViewController.swift
@@ -9,6 +9,42 @@
 import UIKit
 
 class ViewController: UIViewController {
+    private let label = UILabel(frame: .zero)
+    private let button = UIButton(type: .custom)
+    private var count = 0
 
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        view.backgroundColor = .white
+
+        let stack = UIStackView(arrangedSubviews: [label, button])
+        stack.axis = .vertical
+
+        view.addSubview(stack)
+
+        [stack, label, button].forEach {
+            $0.translatesAutoresizingMaskIntoConstraints = false
+        }
+
+        NSLayoutConstraint.activate([
+            stack.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            stack.centerYAnchor.constraint(equalTo: view.centerYAnchor),
+            ])
+
+        button.addTarget(self, action: #selector(pressed(_:)), for: .touchUpInside)
+        button.setTitle("Increment", for: .normal)
+        button.setTitleColor(.blue, for: .normal)
+        updateLabel()
+    }
+
+    @objc func pressed(_ sender: UIButton) {
+        count += 1
+        updateLabel()
+    }
+
+    func updateLabel() {
+        label.text = "\(count)"
+    }
 }
 
