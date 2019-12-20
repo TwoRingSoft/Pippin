81f665598f629afa94ef7e60bd5693826931acf4
Andrew McKnight <andrew@tworingsoft.com>
2019-12-20 00:21:43 -0900

    fixup! ensure equal vertical spacing around text field

--- Sources/PippinLibrary/UIKit/UITableViewCell/TextFieldTableViewCell.swift
+++ Sources/PippinLibrary/UIKit/UITableViewCell/TextFieldTableViewCell.swift
@@ -33,11 +33,14 @@ public class TextFieldTableViewCell: UITableViewCell {
             contentView.addSubview($0)
             $0.translatesAutoresizingMaskIntoConstraints = false
         }
-        
+
+        titleLabel.setContentHuggingPriority(.required, for: .horizontal)
+
         [
             titleLabel.leadingAnchor.constraint(equalTo: contentView.leadingAnchor, constant: CGFloat.horizontalMargin),
             titleLabel.topAnchor.constraint(greaterThanOrEqualTo: contentView.topAnchor, constant: CGFloat.verticalMargin),
-            titleLabel.bottomAnchor.constraint(lessThanOrEqualTo: contentView.bottomAnchor, constant: -CGFloat.verticalMargin),
+            titleLabel.bottomAnchor.constraint(equalTo: contentView.bottomAnchor, constant: -CGFloat.verticalMargin),
+            titleLabel.firstBaselineAnchor.constraint(equalTo: textField.firstBaselineAnchor),
             textField.topAnchor.constraint(greaterThanOrEqualTo: contentView.topAnchor, constant: CGFloat.verticalMargin),
             textField.leadingAnchor.constraint(equalTo: titleLabel.trailingAnchor, constant: CGFloat.horizontalSpacing),
             textField.trailingAnchor.constraint(equalTo: contentView.trailingAnchor, constant: -CGFloat.horizontalMargin),
