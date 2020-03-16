243090fec487f80ef6f10c0d7622c592256d0625
Andrew McKnight <andrew@tworingsoft.com>
2020-03-15 22:04:03 -0800

    refact(DismissableModalViewController): rewrite header layout with UIStackView

--- Sources/PippinLibrary/UIKit/UIViewController/DismissableModalViewController.swift
+++ Sources/PippinLibrary/UIKit/UIViewController/DismissableModalViewController.swift
@@ -25,33 +25,11 @@ public class DismissableModalViewController: UIViewController {
 
         let titleAndCloseButtonView = headerView(tintColor: tintColor, imageBundle: imageBundle, titleFont: titleFont)
         contentView.setContentCompressionResistancePriority(.required, for: .vertical)
-        
-        let margin: CGFloat = 10
-        
-        [titleAndCloseButtonView, contentView].forEach {
-            view.addSubview($0)
-            $0.translatesAutoresizingMaskIntoConstraints = false
-        }
-        var verticalConstraints = Array<NSLayoutConstraint>()
-        if #available(iOS 11.0, *) {
-            verticalConstraints = [
-                titleAndCloseButtonView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: margin),
-                contentView.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor, constant: -margin),
-            ]
-        } else {
-            verticalConstraints = [
-                titleAndCloseButtonView.topAnchor.constraint(equalTo: view.topAnchor, constant: margin),
-                contentView.bottomAnchor.constraint(equalTo: view.bottomAnchor, constant: -margin),
-            ]
-        }
-        let constraints = [
-            titleAndCloseButtonView.leadingAnchor.constraint(equalTo: view.layoutMarginsGuide.leadingAnchor),
-            titleAndCloseButtonView.trailingAnchor.constraint(equalTo: view.layoutMarginsGuide.trailingAnchor),
-            contentView.topAnchor.constraint(equalTo: titleAndCloseButtonView.bottomAnchor, constant: margin),
-            contentView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
-            contentView.trailingAnchor.constraint(equalTo: view.trailingAnchor),
-        ]
-        (constraints + verticalConstraints).forEach { $0.isActive = true }
+
+        let stack = UIStackView(arrangedSubviews: [titleAndCloseButtonView, contentView])
+        stack.axis = .vertical
+        view.addSubview(stack)
+        stack.fillSafeArea(inViewController: self, insets: .all(10))
 
         self.closeBlock = closeBlock
     }
